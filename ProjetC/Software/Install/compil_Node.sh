3e3dc6b056c718c5e0261f1a22d9e1dcc2afaca6
aureliengoux <goux.aurelien@gmail.com>
2017-01-26 11:36:49 +0100

    Add files via upload

--- ProjetC/Software/Install/compil_Node.sh
+++ ProjetC/Software/Install/compil_Node.sh
@@ -1,5 +1,5 @@
 
-g++ -g Source/syntaxConstraints.cpp Source/Lexeur.cpp Source/Lexeme.cpp Source/Node.cpp Source/Node_root.cpp Source/Node_library.cpp Source/Node_entity.cpp Source/Node_architecture.cpp Source/Node_libraryUse.cpp Source/Node_port.cpp Source/testNode.cpp -o Binaries/Node
+g++ -g Source/syntaxConstraints.cpp Source/Lexeur.cpp Source/Lexeme.cpp Source/Node.cpp Source/Node_root.cpp Source/Node_library.cpp Source/Node_entity.cpp Source/Node_architecture.cpp Source/Node_libraryUse.cpp Source/Node_port.cpp Test/testNode.cpp -o Binaries/Node
 
 ./Binaries/Node
 
